42b2464cfa9881d67263151fbf3db95df6193298
RyanFu08 <108840013+RyanFu08@users.noreply.github.com>
2022-12-04 15:28:01 -0800

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,12 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <vector>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <map>
